724c5587329b1b4a070febb2df1474d716aebaaf
Kirill <karalionak_kirill@mail.ru>
2021-11-11 20:13:37 +0300

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Core.TestGenerator.Implementations;
+
+namespace ConsoleApp
+{
+    static class TestProgram
+    {
+        static void Main()
+        {
+            var generator = NUnitTestGenerator.CreateGenerator(builder =>
+            {
+                builder.MaxGenerateFiles = 10;
+                builder.MaxReadFiles = 10;
+                builder.MaxWriteFiles = 10;
+            });
+            generator.GenerateTestsAsync(@"D:\Labs\MPP\4\mpp-lab4-master\ConsoleApp\input",
+                @"D:\Labs\MPP\4\mpp-lab4-master\ConsoleApp\output").Wait();
+        }
+    }
+}
---
+++ Core/TestGenerator/Builders/NUnitTestGeneratorBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Core.TestGenerator.Constants;
+using Core.TestGenerator.Implementations;
+using Core.TestGenerator.Interfaces;
+
+namespace Core.TestGenerator.Builders
+{
+    public class NUnitTestGeneratorBuilder : ITestGeneratorBuilder
+    {
+        private int _maxReadFiles = TestGeneratorBuilderConstants.DefaultMaxReadFiles;
+
+        private int _maxGenerateFiles = TestGeneratorBuilderConstants.DefaultMaxGenerateFiles;
+
+        private int _maxWriteFiles = TestGeneratorBuilderConstants.DefaultMaxWriteFiles;
+
+        public int MaxReadFiles
+        {
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(MaxReadFiles),
+                        "Maximum number of files to read cannot be less than one.");
+                }
+                _maxReadFiles = value;
+            }
+        }
+        
+        public int MaxGenerateFiles
+        {
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(MaxGenerateFiles),
+                        "Maximum number of files to generate cannot be less than one.");
+                }
+                _maxGenerateFiles = value;
+            }
+        }
+        
+        public int MaxWriteFiles
+        {
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(MaxWriteFiles),
+                        "Maximum number of files to write cannot be less than one.");
+                }
+                _maxWriteFiles = value;
+            }
+        }
+
+        public ITestGenerator Build()
+        {
+            return new NUnitTestGenerator(_maxReadFiles, _maxGenerateFiles, _maxWriteFiles);
+        }
+    }
+}
